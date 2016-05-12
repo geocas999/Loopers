827a7b0dd211840f738eb7d2003880afb666cc47
George Caspersson <george@mywaydesign.se>
2016-05-12 13:46:21 +0200

    Create class Vehicle + enum VehicleType

---
+++ Garage2/Models/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public VehicleType VehicleTypes { get; set; }
+        public string RegNr { get; set; }
+        public string ParkingTime { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+    }
+    public enum VehicleType
+    {
+
+    }
+}
